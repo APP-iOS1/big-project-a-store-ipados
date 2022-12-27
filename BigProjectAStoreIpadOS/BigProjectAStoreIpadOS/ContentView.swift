3c7df7ba8963c641e24d9b0556d02802067ba499
Choo Hyun Ho <hyunho4178@gmail.com>
2022-12-27 14:34:56 +0900

    [Chore] ContentView 메뉴아이템 수정
    스플릿탭뷰의 메뉴아이템과 서브아이템 수정

--- BigProjectAStoreIpadOS/BigProjectAStoreIpadOS/ContentView.swift
+++ BigProjectAStoreIpadOS/BigProjectAStoreIpadOS/ContentView.swift
@@ -8,28 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedMenuItemID: MenuItem.ID?
-
+    //전체메뉴(totalMenu)의 id
+    @State private var selectedCategoryId: MenuItem.ID?
+    //선택된 메뉴 아이템
+    @State private var selectedItem: MenuItem?
+    
+    
+    private var menuModel = MenuModel()
+    
     
     
     var body: some View {
-        NavigationSplitView {
-            List(menuItems, selection: $selectedMenuItemID) { menuItem in
-                Text(menuItem.name)
-            }
-        } content: {
-            DetailTestView()
-        } detail: {
-            DetailTestView()
-        }
-    
         
-    }//body
-}
-
-struct DetailTestView: View {
-    var body: some View {
-        Text("DetailView")
+        // 전체 메뉴 -> content: 서브 메뉴 -> detail: 뷰
+        NavigationSplitView {
+            List(menuModel.totalMenuItems, selection: $selectedCategoryId) { item in
+                Text(item.name)
+            }
+            .navigationTitle("스토어 관리")
+            
+        } content: {
+            if let selectedCategoryId,
+               let subMenuItems = menuModel.subMenuItems(for: selectedCategoryId) {
+                
+                List(subMenuItems) { item in
+                    Text(item.name)
+                }
+                .listStyle(.plain)
+                .navigationBarTitleDisplayMode(.inline)
+                
+            } else {
+                Text("Please select a category")
+            }
+        } detail: {
+            if let selectedItem {
+                //
+            } else {
+                Text("Please select an item")
+            }
+        }
     }
 }
 
--- BigProjectAStoreIpadOS/BigProjectAStoreIpadOS/MenuItem.swift
+++ BigProjectAStoreIpadOS/BigProjectAStoreIpadOS/MenuItem.swift
@@ -10,15 +10,59 @@ import Foundation
 struct MenuItem: Identifiable, Hashable {
     var id = UUID()
     var name: String
+    var subMenuItems: [MenuItem]?
+}
+
+struct MenuModel {
     
+    let totalMenuItems = {
+
+        let productManageItems = [MenuItem(name: "상품 등록"),
+                                  MenuItem(name: "상품 수정")]
+        
+        let orderManageItems = [MenuItem(name: "주문내역"),
+                                MenuItem(name: "배송 관리")]
+        
+        let storeManageItems = [MenuItem(name: "스토어 정보 수정"),
+                                MenuItem(name: "폐점 신청")]
+        
+        let inquiryReviewItems = [MenuItem(name: "문의 내역"),
+                                  MenuItem(name: "리뷰 내역")]
+        
+        
+        let menuItems = [
+            MenuItem(name: "상품 관리", subMenuItems: productManageItems),
+            MenuItem(name: "주문 관리", subMenuItems: orderManageItems),
+            MenuItem(name: "스토어 관리", subMenuItems: storeManageItems),
+            MenuItem(name: "매출 현황"),
+            MenuItem(name: "문의 및 리뷰 관리", subMenuItems: productManageItems)
+        ]
+        
+
+        
+        return menuItems
+    }()
+    
+    func subMenuItems(for id: MenuItem.ID) -> [MenuItem]? {
+        guard let menuItem = totalMenuItems.first(where: { $0.id == id }) else {
+            return nil
+        }
+ 
+        return menuItem.subMenuItems
+    }
+ 
+    func menuItem(for categoryID: MenuItem.ID, itemID: MenuItem.ID) -> MenuItem? {
+ 
+        guard let subMenuItems = subMenuItems(for: categoryID) else {
+            return nil
+        }
+ 
+        guard let menuItem = subMenuItems.first(where: { $0.id == itemID }) else {
+            return nil
+        }
+ 
+        return menuItem
+    }
 }
 
 
-let menuItems: [MenuItem] = [
-    MenuItem(name: "상품 관리"),
-    MenuItem(name: "주문 관리"),
-    MenuItem(name: "스토어 관리"),
-    MenuItem(name: "매출 현황"),
-    MenuItem(name: "문의 및 리뷰 관리")
-   
-]
