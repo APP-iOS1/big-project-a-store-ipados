2394a2c4ad43c755973215f65418673d7354a5f2
sue <suekim999@naver.com>
2022-12-27 17:37:44 +0900

    [Add] dummy 데이터 말고 실제로 데이터를 받아오기 위해서 구조체와 VM 만들어봄
    [Add] dummy 데이터 말고 실제로 데이터를 받아오기 위해서 구조체와 VM 만들어봄 (아직 부족)

--- BigProjectAStoreIpadOS/BigProjectAStoreIpadOS/View/OrderManageView/OrderHistoryView.swift
+++ BigProjectAStoreIpadOS/BigProjectAStoreIpadOS/View/OrderManageView/OrderHistoryView.swift
@@ -7,27 +7,34 @@
 
 import SwiftUI
 
-struct OrderHistory: View {
+struct OrderHistoryView: View {
+    // 나중에 customerOrderVM 필요할 것 같아서 일단 만들어보고 있었음
+    @StateObject private var customerOrderVM: CustomerOrderViewModel = CustomerOrderViewModel()
+    
+    // 그리드 6개씩 보여주기 위해
     var columns: [GridItem] = Array(repeating: .init(.flexible()), count: 6)
-    let header: [String] = ["주문 번호", "주문 시간", "주문 상품", "주문 옵션", "수량", "구매확정 여부"]
-    let dummyData: [String] = ["1dfsf", "2022-12-27 13:36",  "맥북 프로", "매직키보드", "2", "N", "2adsfg", "2022-12-28 19:36",  "아이패드", "- ", "1", "N" ]
+    
+    
+    // 주문 내역 부분
+    let orderHistoryHeader: [String] = ["주문 번호", "주문 시간", "주문 상품", "주문 옵션", "수량", "구매확정 여부"]
+    
+    // 임의 주문 데이터
+    let dummyData: [String] = ["1dfsf", "2022-12-27 13:36",  "맥북 프로", "스페이스 그레이", "2", "N", "2adsfg", "2022-12-28 19:36",  "아이패드", "- ", "1", "Y" ]
+    
     
     var body: some View {
-        ScrollView {
+        NavigationStack {
             VStack {
-                Text("주문내역")
-                    .font(.title)
-                    .bold()
-                    
                 
                 LazyVGrid(columns: columns) {
                     
-                    ForEach(header, id: \.self ) { headerItem in
+                    ForEach(orderHistoryHeader, id: \.self ) { headerItem in
                         Text("\(headerItem)")
                     }
+                    .font(.headline)
+                    .padding(.bottom, 6)
                     
 
-                    
                     ForEach(dummyData, id: \.self) { data in
                         Text("\(data)")
                     }
@@ -36,6 +43,8 @@ struct OrderHistory: View {
                 Spacer()
                 
             }//vstack
+            .padding(.top, 20)
+            .navigationTitle(Text("주문 내역"))
         }
     }//body
 }
@@ -44,6 +53,6 @@ struct OrderHistory: View {
 
 struct OrderHistory_Previews: PreviewProvider {
     static var previews: some View {
-        OrderHistory()
+        OrderHistoryView()
     }
 }
