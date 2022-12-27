e15341168c4e015b0d37174388a55d070b4c2313
Sehoon Jeong <wtd120120@gmail.com>
2022-12-27 14:32:56 +0900

    [Add]
    - Product.swift
    - ProductInventoryView.swift

---
+++ BigProjectAStoreIpadOS/BigProjectAStoreIpadOS/Product.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct ProductData: Identifiable, Hashable{
+    var id = UUID()
+    var productName: String
+    var productId: String
+    var productPrice: String
+    var productCount: String
+}
+
+var sampleData:[ProductData] = [
+    ProductData(productName: "키보드", productId: "p1", productPrice: "fnn", productCount: "pc1"),
+    ProductData(productName: "마우스", productId: "p2", productPrice: "bv", productCount: "pc2"),
+    ProductData(productName: "컴퓨터", productId: "p3", productPrice: "y45", productCount: "pc3"),
+    ProductData(productName: "노트북", productId: "p8", productPrice: "656", productCount: "pc4"),
+    ProductData(productName: "맥북", productId: "p9", productPrice: "234", productCount: "pc5"),
+    ProductData(productName: "애플워치", productId: "p11", productPrice: "767", productCount: "pc6"),
+    ProductData(productName: "아이패드", productId: "p12", productPrice: "545", productCount: "pc7"),
+    ProductData(productName: "갤탭", productId: "p13", productPrice: "332", productCount: "pc8")
+    
+]
---
+++ BigProjectAStoreIpadOS/BigProjectAStoreIpadOS/ProductInventoryView.swift
@@ -0,0 +1,176 @@
+//
+//  ProductInventoryView.swift
+//  BigProjectAStoreIpadOS
+//
+//  Created by sehooon on 2022/12/27.
+//
+
+import SwiftUI
+
+struct ProductInventoryView: View {
+    
+    
+    
+    
+    @State private var productCategories = [
+        "상품명",
+        "상품코드",
+        "옵션",
+        "재고",
+        "조정사유"
+    ]
+    
+    
+    var body: some View {
+        VStack(){
+            Text("상품 조회/수정")
+                .font(.largeTitle)
+            
+            Divider()
+            productStatusBar
+            
+            Divider()
+            
+            // MARK: - 상품 조회 기능
+            VStack(alignment: .leading){
+                HStack{
+                    Text("카테고리")
+                        .font(.title3)
+                }
+                Divider()
+
+                HStack{
+                    Text("상품등록일")
+                        .font(.title3)
+                    Spacer()
+                }
+                Divider()
+                HStack{
+                    Text("부족재고")
+                        .font(.title3)
+                    Spacer()
+                }
+                Divider()
+
+            }
+            .padding()
+            
+            
+            // MARK: - 상품목록 View
+            VStack(alignment: .leading){
+                Text("상품 목록 (총 0개)")
+                    .font(.title2)
+                    .padding()
+                Divider()
+                VStack{
+                    Table(sampleData) {
+                        TableColumn("상품명", value:\.productName)
+                        TableColumn("상품코드", value:\.productId)
+                        TableColumn("상세") { Identifiable in
+                            Button {
+                                
+                            } label: {
+                                Text(Identifiable.productName)
+                            }
+                        }
+                    }
+                    .padding()
+                }
+            }
+            
+            Spacer()
+        }
+        
+    }
+}
+extension ProductInventoryView{
+    
+    var productStatusBar: some View{
+        VStack{
+            HStack(alignment: .center ,spacing: 50){
+                HStack{
+                    Image(systemName: "shippingbox.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50,height: 50)
+                    VStack(alignment: .leading){
+                        Text("전체")
+                        Text("0 ")                            .font(.title2) + Text("건")
+                    }
+                    .padding()
+                }
+                
+                HStack{
+                    Image(systemName: "cart.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50,height: 50)
+                    VStack(alignment: .leading){
+                        Text("품절")
+                        Text("0 ")                            .font(.title2) + Text("건")
+                    }
+                    .padding()
+                }
+                
+                HStack{
+                    Image(systemName: "cart.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50,height: 50)
+                    VStack(alignment: .leading){
+                        Text("판매중")
+                        Text("0 ")
+                            .font(.title2) + Text("건")
+                    }
+                    .padding()
+                }
+                
+            }
+            HStack(alignment: .center ,spacing: 50){
+                HStack{
+                    Image(systemName: "shippingbox.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50,height: 50)
+                    VStack(alignment: .leading){
+                        Text("전체")
+                        Text("0 ")
+                            .font(.title2) + Text("건")
+                    }
+                    .padding()
+                }
+                
+                HStack{
+                    Image(systemName: "cart.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50,height: 50)
+                    VStack(alignment: .leading){
+                        Text("품절")
+                        Text("0 ").font(.title2) + Text("건")
+                    }
+                    .padding()
+                }
+                
+                HStack{
+                    Image(systemName: "cart.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50,height: 50)
+                    VStack(alignment: .leading){
+                        Text("판매중")
+                        Text("0 ").font(.title2) + Text("건")
+                    }
+                    .padding()
+                }
+                
+            }
+        }
+    }
+}
+
+struct ProductInventoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductInventoryView()
+    }
+}
