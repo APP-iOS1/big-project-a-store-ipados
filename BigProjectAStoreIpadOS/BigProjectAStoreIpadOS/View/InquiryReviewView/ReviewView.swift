0fb9625866419cbd00de76f28fb961c2b43e43ea
sue <suekim999@naver.com>
2022-12-28 12:02:24 +0900

    [Add] 리뷰작성일 피커 추가
    날라가지마라..

--- BigProjectAStoreIpadOS/BigProjectAStoreIpadOS/View/InquiryReviewView/ReviewView.swift
+++ BigProjectAStoreIpadOS/BigProjectAStoreIpadOS/View/InquiryReviewView/ReviewView.swift
@@ -8,12 +8,28 @@
 import SwiftUI
 
 struct ReviewView: View {
+    @State private var reviewWriteSelection: Int = 1
     var body: some View {
         VStack {
             //리뷰 작성일
             Group {
-                Text("리뷰 작성일")
-                    .font(.title2)
+                HStack {
+                    Text("리뷰 작성일")
+                        .font(.title2)
+                    VStack {
+                        Picker(selection: $reviewWriteSelection, label: Text("리뷰 작성일")) {
+                            Text("오늘").tag(1)
+                            Text("1주일").tag(2)
+                            Text("1개월").tag(3)
+                            Text("3개월").tag(4)
+                            Text("6개월").tag(5)
+                            Text("1년").tag(6)
+                        }
+                        .pickerStyle(.segmented)
+                        .frame(width: 500)
+                        
+                    }
+                }
                 Divider()
             }
             // 리뷰 구분
