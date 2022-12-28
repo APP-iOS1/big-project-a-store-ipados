99f40707329b4382f3de1111c510267f19cd630f
sue <suekim999@naver.com>
2022-12-28 11:56:18 +0900

    [Add] ReviewView 추가

---
+++ BigProjectAStoreIpadOS/BigProjectAStoreIpadOS/View/InquiryReviewView/ReviewView.swift
@@ -0,0 +1,20 @@
+//
+//  ReviewView.swift
+//  BigProjectAStoreIpadOS
+//
+//  Created by Sue on 2022/12/28.
+//
+
+import SwiftUI
+
+struct ReviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ReviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReviewView()
+    }
+}
