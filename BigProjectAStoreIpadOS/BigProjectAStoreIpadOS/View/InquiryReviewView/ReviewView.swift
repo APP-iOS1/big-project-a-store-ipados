c032b5470d45dc4b0f8a294cff7eb9a6af22761a
sue <suekim999@naver.com>
2022-12-28 11:59:34 +0900

    [Add] 각 세부 내용 추가

--- BigProjectAStoreIpadOS/BigProjectAStoreIpadOS/View/InquiryReviewView/ReviewView.swift
+++ BigProjectAStoreIpadOS/BigProjectAStoreIpadOS/View/InquiryReviewView/ReviewView.swift
@@ -9,8 +9,45 @@ import SwiftUI
 
 struct ReviewView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        VStack {
+            //리뷰 작성일
+            Group {
+                Text("리뷰 작성일")
+                    .font(.title2)
+                Divider()
+            }
+            // 리뷰 구분
+            Group {
+                Text("리뷰 구분")
+                    .font(.title2)
+                Divider()
+            }
+            //리뷰 타입
+            Group {
+                Text("리뷰 타입")
+                    .font(.title2)
+                Divider()
+            }
+            // 카테고리
+            Group {
+                Text("카테고리")
+                    .font(.title2)
+                Divider()
+            }
+            // 채널
+            Group {
+                Text("채널")
+                    .font(.title2)
+                Divider()
+            }
+            // 리뷰 조건
+            Group {
+                Text("리뷰 조건")
+                    .font(.title2)
+                Divider()
+            }
+        }//vstack
+    }//body
 }
 
 struct ReviewView_Previews: PreviewProvider {
