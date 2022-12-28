3fe51d86f0bce901829be4cb79244e78e618707b
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2022-12-28 10:01:29 +0900

    [Fix] Git 충돌 해결 (2)
    **Git 충돌 해결 (2)**

---
+++ BigProjectAStoreIpadOS/BigProjectAStoreIpadOS/View/SettingsView.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsView.swift
+//  BigProjectAStoreIpadOS
+//
+//  Created by 최한호 on 2022/12/28.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        VStack {
+            Button {
+                
+            } label: {
+                Text("로그아웃")
+            }
+
+        }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
