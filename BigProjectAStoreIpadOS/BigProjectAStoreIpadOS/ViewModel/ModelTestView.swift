313147377b22414deb2193d978df05570bcfea8a
valseLee <sollleky72@gmail.com>
2023-01-02 16:03:32 +0900

    [Add] 모델 테스트용 뷰, 회원가입 모델 파일 생성

---
+++ BigProjectAStoreIpadOS/BigProjectAStoreIpadOS/ViewModel/ModelTestView.swift
@@ -0,0 +1,20 @@
+//
+//  ModelTestView.swift
+//  BigProjectAStoreIpadOS
+//
+//  Created by 이승준 on 2023/01/02.
+//
+
+import SwiftUI
+
+struct ModelTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ModelTestView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModelTestView()
+    }
+}
---
+++ BigProjectAStoreIpadOS/BigProjectAStoreIpadOS/ViewModel/Store/UserAuthenticationModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserAuthenticationModel.swift
+//  BigProjectAStoreIpadOS
+//
+//  Created by 이승준 on 2023/01/02.
+//
+
+import Foundation
