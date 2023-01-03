df0476aabeede3e36fd2ff06862700018bbb895f
valseLee <sollleky72@gmail.com>
2023-01-03 16:31:27 +0900

    [Feat] 회원가입 후 회원 생성하는 메소드 생성, 뷰 테스트 완료

--- BigProjectAStoreIpadOS/BigProjectAStoreIpadOS/ViewModel/ModelTestView.swift
+++ BigProjectAStoreIpadOS/BigProjectAStoreIpadOS/ViewModel/ModelTestView.swift
@@ -15,8 +15,7 @@ struct ModelTestView: View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
 			.task {
 				
-				await model.updateStoreInfo(with: "UaKxPUQgK7bqmJb9fRbN95BZ21M2",
-											by: .storeName(value: "이름바꿀거임"), .isSubmitted(value: true), .registerDate(value: Date.now))
+				await model.createStoreInfo(with: StoreInfo(storeId: "1234", storeEmail: "test@ma.com", registerDate: Date.getKoreanNowTimeString(), reportingCount: 0))
 			}
     }
 }
