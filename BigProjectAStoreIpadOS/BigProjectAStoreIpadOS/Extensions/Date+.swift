6b6cc862019992166b09fa8b6b29f49f15c5aa59
valseLee <sollleky72@gmail.com>
2023-01-03 16:30:46 +0900

    [Feat] 날짜 구조체 생성, 스트링으로 날짜 만들어서 반환하는 메소드 구현

--- BigProjectAStoreIpadOS/BigProjectAStoreIpadOS/Extensions/Date+.swift
+++ BigProjectAStoreIpadOS/BigProjectAStoreIpadOS/Extensions/Date+.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+extension Date {
+	static func getKoreanNowTimeString() -> String {
+		let dateFormatter = DateFormatter()
+		dateFormatter.locale = Locale(identifier: "ko-KR")
+		dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+		return dateFormatter.string(from: Date.now)
+	}
+}
+
